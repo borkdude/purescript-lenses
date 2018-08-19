2a6d918297d1be6e746a27ef8228f6e612804031
Michiel Borkent <michielborkent@gmail.com>
2018-08-19 23:59:06 +0200

    Exercises 1.4

--- src/ProductExercises.purs
+++ src/ProductExercises.purs
@@ -39,3 +39,13 @@ _object =
   lens getter setter where
     getter = _.object
     setter whole new = whole { object = new }
+
+-- or shorter:
+-- _object = lens _.object (_ { object = _ })
+
+stringified = over _2 show both
+
+_bothObject = _2 <<< _object
+
+-- exercise3 :: Tuple String String
+exercise3 = over _2 (view _object) both
