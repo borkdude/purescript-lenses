1d1316ea27b696c1ee57549e5fd3cace13630d12
Michiel Borkent <michielborkent@gmail.com>
2018-08-23 21:18:08 +0200

    Traversal Types

--- src/TraversalStart.purs
+++ src/TraversalStart.purs
@@ -1,7 +1,5 @@
 module TraversalStart where
 
-{-
-
 import Prelude
 import Data.Tuple (Tuple)
 import Data.Maybe (Maybe)
@@ -9,14 +7,60 @@ import Data.Lens (Traversal', Traversal, _1, traversed)
 import Data.Traversable (class Traversable)
 import Data.Lens.At (class At, at)
 import Data.Map (Map)
+import Data.Lens.Internal.Wander
+import Data.Profunctor.Strong
 
+-- rewrite without using map, use over
+-- view traversed $ map Additive [1, 2, 3]
+-- view traversed $ over traversed Additive [1, 2, 3]
 
-_trav_trav :: 
+-- Exercise: Use view to convert [["1"], ["2", "3"]] to "123". Then use view to convert [[1], [2, 3]] to Additive 10.
+
+-- _trav_trav = traversed <<< traversed
+-- view _trav_trav [["1"], ["2", "3"]]
+-- view _trav_trav (over _trav_trav Additive [[1], [2,3]])
+
+-- _trav_1 = traversed <<< _1
+-- example = [ Tuple 1 2, Tuple 3 4 ]
+-- Exercise: Predict the result of preview _trav_1 example. The answer is in src/Traversal.purs.
+-- probably Just 1? Yep
+
+-- Composition exercises
+
+-- Add type annotations
+
+-- t7 = trav1
+-- t11 = trav2
+-- t8 = wander
+-- t10 = a
+-- t9 = b
+
+-- All optics are profunctor functions of this form: profunctor a b ->
+-- profunctor s t
+
+-- Wander is the clue that we’re looking at a Traversal, just as Strong is the
+-- clue we’re looking at a Lens.
+
+-- _trav_trav :: forall a trav2 trav1 profunctor b.
+--               Traversable trav1 => Wander profunctor => Traversable trav2 =>
+--               profunctor a b -> profunctor (trav1 (trav2 a)) (trav1 (trav2 b))
+--               -- dus we gaan van een Wander a b naar een Wander van een geneste
+--               -- traversable a naar een geneste traversable b
+-- simplified:
+_trav_trav :: forall a b trav1 trav2.
+              Traversable trav1 => Traversable trav2 =>
+              Traversal (trav1 (trav2 a)) (trav1 (trav2 b)) a b
+
 _trav_trav = traversed <<< traversed
 
-
-_1_trav :: 
+_1_trav ::
+  forall a b traversable _2_.
+  Traversable traversable =>
+  Traversal (Tuple (traversable a) _2_) (Tuple (traversable b) _2_) a b
 _1_trav = _1 <<< traversed
+
+{-
+
 
 _trav_at3 :: 
 _trav_at3 = traversed <<< at 3
@@ -28,4 +72,5 @@ _at3_trav_1' :: forall a _1_ .
 _at3_trav_1' = at 3 <<< traversed <<< _1
 
 -}
+
 
