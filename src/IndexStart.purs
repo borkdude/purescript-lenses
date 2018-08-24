765c5605cd5adc43c9ba027496d20ea58f47833b
Michiel Borkent <michielborkent@gmail.com>
2018-08-24 11:52:24 +0200

    ix exercises

--- src/IndexStart.purs
+++ src/IndexStart.purs
@@ -1,9 +1,5 @@
 module IndexStart where
 
--- Exercises for the chapter on `Index` optics.
-
-{- 
-
 import Prelude
 
 import Data.Maybe
@@ -13,18 +9,24 @@ import Data.Lens.Index
 import Data.Traversable
 import Data.Tuple
 
-
-_1_ix1 :: 
+_1_ix1 :: forall indexed a z.
+          Index indexed Int a =>
+          Traversal' (Tuple indexed z) a
 _1_ix1 = _1 <<< ix 1
 
-
-_ix1_1 :: 
+_ix1_1 :: forall indexed a z.
+          Index indexed Int (Tuple a z) =>
+          Traversal' indexed a
 _ix1_1 = ix 1 <<< _1
 
-
-_ix1_at1 :: 
+_ix1_at1 :: forall indexed keyed a.
+            Index indexed Int keyed =>
+            At keyed Int a =>
+            Traversal' indexed (Maybe a)
 _ix1_at1 = ix 1 <<< at 1
 
-_ix1_ix1 :: 
+_ix1_ix1 :: forall ix1 ix2 a.
+            Index ix1 Int ix2 =>
+            Index ix2 Int a =>
+            Traversal' ix1 a
 _ix1_ix1 = ix 1 <<< ix 1
--}
